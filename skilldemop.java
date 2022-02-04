6f26d60962bef05bbedf98c6da4588e7dc5ba563
YueSteveYin <yuy045@ucsd.edu>
2022-02-03 18:29:39 -0800

    failed test

--- skilldemop.java
+++ skilldemop.java
@@ -1,1 +1,7 @@
-
+public class skilldemop
+{
+    public static int number(int n)
+    {
+        return n;
+    }
+}
---
+++ skilldemopTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemopTest {
+    @Test
+    public void addition() {
+        assertEquals(2, skilldemop.number(1));
+    }
+}
